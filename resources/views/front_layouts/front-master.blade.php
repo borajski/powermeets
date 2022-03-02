03dd7daa0e480180d1e729c244d3d35f8a36361f
borajski <sknezev@inet.hr>
2022-03-02 17:14:29 +0100

    front design

--- resources/views/front_layouts/front-master.blade.php
+++ resources/views/front_layouts/front-master.blade.php
@@ -4,9 +4,20 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>Powerlifting Meets</title>
-    <meta name="description" content="">
+    <title>Powerlifting Software</title>
+    <meta name="description" content="POWERMEETS-a modern powerlifting software-Web platform for organizing and managing powerlifting competitions.">
+    <meta name="keywords" content="powerlifting software,powerlifting application,web application,powerlifting meets,meets,nominations,results,entry lists,competition">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta property="og:title" content="PowerMeets-a modern powerlifting software">
+    <meta property="og:type" content="website">
+    <meta property="og:description" content="Web platform for organizing and managing powerlifting competitions.">
+    <meta property="og:url" content="/">
+    <meta name="twitter:card" content="summary_large_image">
+    <meta property="og:image" content="{{asset('images/front/powermeets.jpg')}}" />
+    <meta name="robots" content="index,follow" /> 
+    <link rel="canonical" href="/">
+    <link rel="icon" type="text/svg+xml" href="{{asset('images/front/favicon.svg')}}">
+    <link rel="icon" href="{{asset('images/front/favicon.png')}}">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="preconnect" href="https://fonts.googleapis.com">
