674f812a6369d098884268819fcd704b876a3fac
borajski <sknezev@inet.hr>
2022-06-16 15:10:41 +0200

    update

--- public/js/front/front-scripts.js
+++ public/js/front/front-scripts.js
@@ -27,11 +27,11 @@ function getNominations(disciplina)
       ispis = '<h3 class="mt-3 mb-3 text-center"><strong>' + odgovori.ispis.toUpperCase() + '</strong></h3>';
       if ( odgovori.nominacije_m != "")
       {
-        var tablica ='<table class="table table-hover bg-light shadow"><thead class="thead"><tr><th>R.br.</th><th>Ime i prezime</th><th>Klub</th><th>Dob</th><th>Država</th></tr></thead><tbody>';
+        var tablica ='<table class="table table-hover bg-light shadow"><thead class="thead"><tr><th>#</th><th>Name and surname</th><th>Team</th><th>Age</th><th>Country</th></tr></thead><tbody>';
         var body = "";
         var i = 0;
       for (var j in odgovori.tezinske_m) {
-        body += '<tr><td class="text-center text-light bg-dark" colspan="5">Kategorija:&nbsp;'+ odgovori.tezinske_m[j] + 'kg</td></tr>';
+        body += '<tr><td class="text-center text-light bg-dark" colspan="5">Category:&nbsp;'+ odgovori.tezinske_m[j] + 'kg</td></tr>';
         for (var key in odgovori.nominacije_m) {
         if (odgovori.nominacije_m[key].kategorijat == odgovori.tezinske_m[j])
         {
@@ -50,11 +50,11 @@ function getNominations(disciplina)
 }    
 if ( odgovori.nominacije_f != "")
 {  
-   var tablica ='<table class="table table-hover bg-light shadow"><thead class="thead"><tr><th>R.br.</th><th>Ime i prezime</th><th>Klub</th><th>Dob</th><th>Država</th></tr></thead><tbody>';
+   var tablica ='<table class="table table-hover bg-light shadow"><thead class="thead"><tr><th>#</th><th>Name and surname</th><th>Team</th><th>Age</th><th>Country</th></tr></thead><tbody>';
     var body = "";
     var i = 0;
   for (var j in odgovori.tezinske_f) {
-    body += '<tr><td class="text-center text-light bg-dark" colspan="5">Kategorija:&nbsp;'+ odgovori.tezinske_f[j] + 'kg</td></tr>';
+    body += '<tr><td class="text-center text-light bg-dark" colspan="5">Category:&nbsp;'+ odgovori.tezinske_f[j] + 'kg</td></tr>';
     for (var key in odgovori.nominacije_f) {
     if (odgovori.nominacije_f[key].kategorijat == odgovori.tezinske_f[j])
     {
@@ -95,7 +95,7 @@ function getGroups(disciplina)
         var tablica ='<table class="table table-hover bg-light shadow"><thead class="thead"><tr><th>R.br.</th><th>Ime i prezime</th><th>Dob</th><th>Kategorija</th><th>Spol</th></tr></thead><tbody>';
         var body = "";
        for (var j in odgovori.grupe) {
-        body += '<tr><td class="text-center text-light bg-dark" colspan="5">Grupa:&nbsp;'+ odgovori.grupe[j] + '</td></tr>';
+        body += '<tr><td class="text-center text-light bg-dark" colspan="5">Group:&nbsp;'+ odgovori.grupe[j] + '</td></tr>';
         im = 0; 
         for (var key in odgovori.natjecatelji) {
             if (odgovori.natjecatelji[key].flight == odgovori.grupe[j])
